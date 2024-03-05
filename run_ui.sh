6ff91411fb77ba7c70376800c9fafe6b9dde0cf5
Suchintan <suchintan@users.noreply.github.com>
2024-03-05 13:57:18 -0500

    Update run ui script to allow presets + update streamlit to accept them (#30)

--- run_ui.sh
+++ run_ui.sh
@@ -1,3 +1,5 @@
+
+
 source "$(poetry env info --path)/bin/activate"
 python scripts/tracking.py skyvern-oss-run-ui
-streamlit run streamlit_app/visualizer/streamlit.py
+streamlit run streamlit_app/visualizer/streamlit.py -- $@
