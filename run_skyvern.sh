72d59e922ac313fb9719fa625f7eccf1e5b026e7
unaisshemim <unaisshemim@gmail.com>
2024-04-07 20:39:05 +0530

    UI and server can run in differnct ports and merged into single file

--- run_skyvern.sh
+++ run_skyvern.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+# Activate Python virtual environment
+source "$(poetry env info --path)/bin/activate"
+
+# Kill any process running on port 8000
 kill $(lsof -t -i :8000)
 
+# Check if .env file exists, if not, copy .env.example
 if [ ! -f .env ]; then
   cp .env.example .env
-  echo "Please add your api keys to the .env file."
+  echo "Please add your API keys to the .env file."
 fi
+
+# Activate Python virtual environment (again)
 source "$(poetry env info --path)/bin/activate"
+
+# Install project dependencies using Poetry
 poetry install
+
+# Execute database migration checks
 ./run_alembic_check.sh
-poetry run python -m skyvern.forge
+
+# Run Streamlit app
+streamlit run streamlit_app/visualizer/streamlit.py -- $@
+
+# Run Python module skyvern.forge-[optimize] using Poetry
+poetry run python -m skyvern.forge-[optimize]
