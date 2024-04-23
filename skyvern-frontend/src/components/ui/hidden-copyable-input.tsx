a56d292bd3be33f5a1d4eed701144a259f21988f
ykeremy <keremyilmazcs@gmail.com>
2024-04-23 19:20:01 +0000

    🔄 synced local 'skyvern-frontend/src/' with remote 'skyvern-frontend/src/'

---
+++ skyvern-frontend/src/components/ui/hidden-copyable-input.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Button } from "./button";
+import { Input } from "./input";
+import { CheckIcon, CopyIcon } from "@radix-ui/react-icons";
+
+type Props = {
+  value: string;
+};
+
+function HiddenCopyableInput({ value }: Props) {
+  const [hidden, setHidden] = useState(true);
+  const [copied, setCopied] = useState(false);
+
+  const buttonText = hidden ? "Reveal" : copied ? "Copied" : "Copy";
+  const inputValue = hidden ? "**** **** **** ****" : value;
+
+  return (
+    <div className="relative w-full">
+      <Input value={inputValue} className="h-10" readOnly />
+      <div className="absolute flex inset-y-0 items-center right-1">
+        <Button
+          size="sm"
+          variant="secondary"
+          className="cursor-pointer"
+          onClick={async () => {
+            if (hidden) {
+              setHidden(false);
+              return;
+            }
+            await navigator.clipboard.writeText(value);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 3000);
+          }}
+        >
+          {!hidden && !copied && <CopyIcon className="mr-2 h-4 w-4" />}
+          {!hidden && copied && <CheckIcon className="mr-2 h-4 w-4" />}
+          {buttonText}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export { HiddenCopyableInput };
