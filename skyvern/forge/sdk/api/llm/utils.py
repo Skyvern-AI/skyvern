7f58e6f66bb3e9efd38755c66f0bad6f72a7e031
otmane <otmanebenazzou.pro@gmail.com>
2024-05-09 17:02:25 +0200

    linting

--- skyvern/forge/sdk/api/llm/utils.py
+++ skyvern/forge/sdk/api/llm/utils.py
@@ -30,16 +30,16 @@ async def llm_messages_builder(
                     },
                 }
             )
-    #Anthropic models seems to struggle to always output a valid json object so we need to prefill the response to force it:
+    # Anthropic models seems to struggle to always output a valid json object so we need to prefill the response to force it:
     if SettingsManager.get_settings().ENABLE_ANTHROPIC:
-        return [{"role": "user", "content": messages},{"role": "assistant", "content": "{"}]
+        return [{"role": "user", "content": messages}, {"role": "assistant", "content": "{"}]
     return [{"role": "user", "content": messages}]
 
 
 def parse_api_response(response: litellm.ModelResponse) -> dict[str, str]:
     try:
         content = response.choices[0].message.content
-        #Since we prefilled Anthropic response with "{" we need to add it back to the response to have a valid json object:
+        # Since we prefilled Anthropic response with "{" we need to add it back to the response to have a valid json object:
         if SettingsManager.get_settings().ENABLE_ANTHROPIC:
             content = "{" + content
         content = try_to_extract_json_from_markdown_format(content)
@@ -51,14 +51,15 @@ def parse_api_response(response: litellm.ModelResponse) -> dict[str, str]:
         raise InvalidLLMResponseFormat(str(response)) from e
 
 def replace_useless_text_around_json(input_string):
-    first_occurrence_of_brace = input_string.find('{')
-    last_occurrence_of_brace = input_string.rfind('}')
-    return input_string[first_occurrence_of_brace:last_occurrence_of_brace + 1]
+    first_occurrence_of_brace = input_string.find("{")
+    last_occurrence_of_brace = input_string.rfind("}")
+    return input_string[first_occurrence_of_brace : last_occurrence_of_brace + 1]
 
 def try_to_extract_json_from_markdown_format(text):
-    pattern = r'```json\s*(.*?)\s*```'
+    pattern = r"```json\s*(.*?)\s*```"
     match = re.search(pattern, text, re.DOTALL)
     if match:
         return match.group(1)
     else:
         return text
+    
