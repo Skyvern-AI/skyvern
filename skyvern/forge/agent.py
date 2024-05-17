2e86b41beb53135684c3e786153ae4b08e7f4297
LawyZheng <lawyzheng1106@gmail.com>
2024-05-17 11:39:46 +0800

    remove HTML experiment (#335)

--- skyvern/forge/agent.py
+++ skyvern/forge/agent.py
@@ -676,13 +676,8 @@ class ForgeAgent:
             LOG.info("Using Claude3 Sonnet prompt template for action extraction")
             prompt_template = "extract-action-claude3-sonnet"
 
-        element_tree_format = ElementTreeFormat.JSON
-        if app.EXPERIMENTATION_PROVIDER.is_feature_enabled_cached(
-            "USE_HTML_ELEMENT_TREE",
-            task.workflow_run_id or task.task_id,
-            properties={"organization_id": task.organization_id},
-        ):
-            element_tree_format = ElementTreeFormat.HTML
+        # TODO: we only use HTML element for now, introduce a way to swith in the future
+        element_tree_format = ElementTreeFormat.HTML
         LOG.info(
             "Building element tree",
             task_id=task.task_id,
