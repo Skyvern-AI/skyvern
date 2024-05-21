b16c889e1b5fc7fffe4081e1ff5b3bed9a1eb8f6
LawyZheng <lawyzheng1106@gmail.com>
2024-05-21 10:44:34 +0800

    maxlength of non-interactable increase

--- skyvern/webeye/scraper/domUtils.js
+++ skyvern/webeye/scraper/domUtils.js
@@ -758,8 +758,10 @@ function buildTreeFromBody() {
           }
         }
 
-        // character length limit for non-interactable elements should be 100
-        if (textContent && textContent.length <= 100) {
+        // character length limit for non-interactable elements should be 5000
+        // we don't use element context in HTML format,
+        // so we need to make sure we parse all text node to avoid missing text in HTML.
+        if (textContent && textContent.length <= 5000) {
           var elementObj = buildElementObject(element, false);
           elements.push(elementObj);
           if (parentId === null) {
