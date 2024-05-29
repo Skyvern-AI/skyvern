0b86f9fb38cd0c4a4e341bda209ffd2113b32584
Salih Altun <salih@skyvern.com>
2024-05-29 21:49:08 +0300

    Fix success and warning css (#390)

--- skyvern-frontend/tailwind.config.js
+++ skyvern-frontend/tailwind.config.js
@@ -35,6 +35,14 @@ module.exports = {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        warning: {
+          DEFAULT: "hsl(var(--warning))",
+          foreground: "hsl(var(--warning-foreground))",
+        },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
