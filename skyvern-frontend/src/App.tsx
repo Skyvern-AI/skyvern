81538a74e1034091c0d79fdbbd1899ca2c6baaa0
Salih Altun <salih@skyvern.com>
2024-06-07 11:19:36 +0300

    add posthog (#440)

--- skyvern-frontend/src/App.tsx
+++ skyvern-frontend/src/App.tsx
@@ -4,13 +4,24 @@ import { router } from "./router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./api/QueryClient";
 
+import { PostHogProvider } from "posthog-js/react";
+
+const postHogOptions = {
+  api_host: "https://app.posthog.com",
+};
+
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark">
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <PostHogProvider
+      apiKey="phc_bVT2ugnZhMHRWqMvSRHPdeTjaPxQqT3QSsI3r5FlQR5"
+      options={postHogOptions}
+    >
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark">
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
 
