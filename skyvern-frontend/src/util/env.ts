3982fc84bdd52f2189e968b8197e855eb562e711
Muhammed Salih Altun <muhammedsalihaltun@gmail.com>
2024-06-13 18:22:25 +0300

    Turn environment variable errors to warnings
      Let's not put errors in console for values that are optional

--- skyvern-frontend/src/util/env.ts
+++ skyvern-frontend/src/util/env.ts
@@ -1,13 +1,13 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL as string;
 
 if (!apiBaseUrl) {
-  console.error("apiBaseUrl environment variable was not set");
+  console.warn("apiBaseUrl environment variable was not set");
 }
 
 const environment = import.meta.env.VITE_ENVIRONMENT as string;
 
 if (!environment) {
-  console.error("environment environment variable was not set");
+  console.warn("environment environment variable was not set");
 }
 
 const envCredential: string | null =
@@ -16,7 +16,7 @@ const envCredential: string | null =
 const artifactApiBaseUrl = import.meta.env.VITE_ARTIFACT_API_BASE_URL;
 
 if (!artifactApiBaseUrl) {
-  console.error("artifactApiBaseUrl environment variable was not set");
+  console.warn("artifactApiBaseUrl environment variable was not set");
 }
 
 export { apiBaseUrl, environment, envCredential, artifactApiBaseUrl };
