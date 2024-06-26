47c1a6f0e879cce7645b5ad9061dab9ba33b6c2d
Kerem Yilmaz <kerem@skyvern.com>
2024-06-26 15:57:24 -0700

    Task cancelling frontend (#522)

--- skyvern-frontend/src/routes/tasks/detail/TaskDetails.tsx
+++ skyvern-frontend/src/routes/tasks/detail/TaskDetails.tsx
@@ -1,17 +1,36 @@
 import { getClient } from "@/api/AxiosClient";
 import { Status, TaskApiResponse } from "@/api/types";
 import { StatusBadge } from "@/components/StatusBadge";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
 import { useCredentialGetter } from "@/hooks/useCredentialGetter";
 import { cn } from "@/util/utils";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { NavLink, Outlet, useParams } from "react-router-dom";
 
 function TaskDetails() {
   const { taskId } = useParams();
   const credentialGetter = useCredentialGetter();
+  const queryClient = useQueryClient();
 
   const {
     data: task,
@@ -36,6 +55,35 @@ function TaskDetails() {
     placeholderData: keepPreviousData,
   });
 
+  const cancelTaskMutation = useMutation({
+    mutationFn: async () => {
+      const client = await getClient(credentialGetter);
+      return client
+        .post(`/tasks/${taskId}/cancel`)
+        .then((response) => response.data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["task", taskId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["tasks"],
+      });
+      toast({
+        variant: "success",
+        title: "Task Canceled",
+        description: "The task has been successfully canceled.",
+      });
+    },
+    onError: (error) => {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: error.message,
+      });
+    },
+  });
+
   if (taskIsError) {
     return <div>Error: {taskError?.message}</div>;
   }
@@ -52,6 +100,9 @@ function TaskDetails() {
       />
     </div>
   ) : null;
+
+  const taskIsRunningOrQueued =
+    task?.status === Status.Running || task?.status === Status.Queued;
 
   const showFailureReason =
     task?.status === Status.Failed ||
@@ -70,13 +121,47 @@ function TaskDetails() {
 
   return (
     <div className="flex flex-col gap-8">
-      <div className="flex items-center gap-4">
-        <span className="text-lg">{taskId}</span>
-        {taskIsLoading ? (
-          <Skeleton className="w-28 h-8" />
-        ) : task ? (
-          <StatusBadge status={task?.status} />
-        ) : null}
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-4">
+          <span className="text-lg">{taskId}</span>
+          {taskIsLoading ? (
+            <Skeleton className="w-28 h-8" />
+          ) : task ? (
+            <StatusBadge status={task?.status} />
+          ) : null}
+        </div>
+        {taskIsRunningOrQueued && (
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="destructive">Cancel</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Are you sure?</DialogTitle>
+                <DialogDescription>
+                  Are you sure you want to cancel this task?
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="secondary">Back</Button>
+                </DialogClose>
+                <Button
+                  variant="destructive"
+                  onClick={() => {
+                    cancelTaskMutation.mutate();
+                  }}
+                  disabled={cancelTaskMutation.isPending}
+                >
+                  {cancelTaskMutation.isPending && (
+                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Cancel Task
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        )}
       </div>
       {taskIsLoading ? (
         <div className="flex items-center gap-2">
