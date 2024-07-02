7763134f8d43fd7e652c15b7dc0b9802c4dd747b
Muhammed Salih Altun <muhammedsalihaltun@gmail.com>
2024-07-02 16:39:46 +0300

    populate env file in run_ui if its not there

--- run_ui.sh
+++ run_ui.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+kill $(lsof -t -i :8080)
+
 cd skyvern-frontend
+
+if [ ! -f .env ]; then
+  cp .env.example .env
+  echo "Please add your api keys to the skyvern-frontend/.env file."
+fi
+
 npm install --silent
 npm run start
