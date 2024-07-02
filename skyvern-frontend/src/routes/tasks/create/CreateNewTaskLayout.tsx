42e299a9d9ff4defaef0550ec0d4a438d1f9c355
Kerem Yilmaz <kerem@skyvern.com>
2024-07-02 08:02:45 -0700

    Add container util + ghost footer (#539)

--- skyvern-frontend/src/routes/tasks/create/CreateNewTaskLayout.tsx
+++ skyvern-frontend/src/routes/tasks/create/CreateNewTaskLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function CreateNewTaskLayout() {
   return (
-    <main className="max-w-6xl mx-auto px-8">
+    <main className="container mx-auto px-8">
       <Outlet />
     </main>
   );
