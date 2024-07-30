5a1b01fca89ae29d68b4eceb3fc3bc0c15e8982c
Shuchang Zheng <wintonzheng0325@gmail.com>
2024-07-30 09:09:52 -0700

    move tracer to skyvern init

--- skyvern/__init__.py
+++ skyvern/__init__.py
@@ -1,3 +1,13 @@
+from ddtrace import tracer
+from ddtrace.filters import FilterRequestsOnUrl
+
 from skyvern.forge.sdk.forge_log import setup_logger
 
+tracer.configure(
+    settings={
+        "FILTERS": [
+            FilterRequestsOnUrl(r"http://.*/heartbeat$"),
+        ],
+    },
+)
 setup_logger()
