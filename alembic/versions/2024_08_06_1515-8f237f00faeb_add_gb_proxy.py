6b1c03fcc98de60e402d27166c2131d56e07ed46
Kerem Yilmaz <keremyilmazcs@gmail.com>
2024-08-06 18:15:32 +0300

    Add GB proxy support

---
+++ alembic/versions/2024_08_06_1515-8f237f00faeb_add_gb_proxy.py
@@ -0,0 +1,26 @@
+"""Add GB proxy
+
+Revision ID: 8f237f00faeb
+Revises: c5ed5a3a14eb
+Create Date: 2024-08-06 15:15:15.369986+00:00
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '8f237f00faeb'
+down_revision: Union[str, None] = 'c5ed5a3a14eb'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("ALTER TYPE proxylocation ADD VALUE 'RESIDENTIAL_GB'")
+
+
+def downgrade() -> None:
+    pass
