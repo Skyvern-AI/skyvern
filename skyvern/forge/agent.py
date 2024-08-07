3f4c913d2692d8c6a63ec73ba66832700c3d4b48
Kerem Yilmaz <keremyilmazcs@gmail.com>
2024-08-07 15:08:40 +0300

    Remove old claude prompt for action extraction

--- skyvern/forge/agent.py
+++ skyvern/forge/agent.py
@@ -960,14 +960,6 @@ class ForgeAgent:
         current_url = (
             await browser_state.page.evaluate("() => document.location.href") if browser_state.page else starting_url
         )
-        prompt_template = "extract-action"
-        if app.EXPERIMENTATION_PROVIDER.is_feature_enabled_cached(
-            "USE_CLAUDE3_SONNET",
-            task.workflow_run_id or task.task_id,
-            properties={"organization_id": task.organization_id},
-        ):
-            LOG.info("Using Claude3 Sonnet prompt template for action extraction")
-            prompt_template = "extract-action-claude3-sonnet"
 
         # TODO: we only use HTML element for now, introduce a way to switch in the future
         element_tree_format = ElementTreeFormat.HTML
@@ -981,7 +973,7 @@ class ForgeAgent:
         element_tree_in_prompt: str = scraped_page.build_element_tree(element_tree_format)
         final_navigation_payload = self._build_navigation_payload(task)
         extract_action_prompt = prompt_engine.load_prompt(
-            prompt_template,
+            "extract-action",
             navigation_goal=navigation_goal,
             navigation_payload_str=json.dumps(final_navigation_payload),
             starting_url=starting_url,
