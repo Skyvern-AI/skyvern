8e81b3928532745d6c067fdb5ba9be35925674cc
Shuchang Zheng <wintonzheng0325@gmail.com>
2024-09-10 23:44:02 -0700

    make totp code fetch more reliable

--- skyvern/webeye/actions/handler.py
+++ skyvern/webeye/actions/handler.py
@@ -2118,6 +2118,8 @@ async def poll_verification_code(
     if not org_token:
         LOG.error("Failed to get organization token when trying to get verification code")
         return None
+    # wait for 40 seconds to get the verification code before actually polling
+    await asyncio.sleep(SettingsManager.get_settings().VERIFICATION_CODE_INITIAL_WAIT_TIME_SECS)
     while True:
         # check timeout
         if datetime.utcnow() > timeout_datetime:
