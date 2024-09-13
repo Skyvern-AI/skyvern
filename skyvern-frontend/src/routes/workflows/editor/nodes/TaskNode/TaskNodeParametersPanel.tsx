d435f1bffa75b61d061e35e15a3c012b3ceb7c92
Kerem Yilmaz <kerem@skyvern.com>
2024-09-13 07:47:57 -0700

    Change how parameters are displayed in task node (#827)

--- skyvern-frontend/src/routes/workflows/editor/nodes/TaskNode/TaskNodeParametersPanel.tsx
+++ skyvern-frontend/src/routes/workflows/editor/nodes/TaskNode/TaskNodeParametersPanel.tsx
@@ -1,5 +1,13 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { useWorkflowParametersState } from "../../useWorkflowParametersState";
+import { Label } from "@/components/ui/label";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 
 type Props = {
   availableOutputParameters: Array<string>;
@@ -16,34 +24,55 @@ function TaskNodeParametersPanel({
   const keys = workflowParameters
     .map((parameter) => parameter.key)
     .concat(availableOutputParameters);
+
+  function toggleParameter(key: string) {
+    if (parameters.includes(key)) {
+      onParametersChange(
+        parameters.filter((parameterKey) => parameterKey !== key),
+      );
+    } else {
+      onParametersChange([...parameters, key]);
+    }
+  }
+
   return (
     <div className="space-y-4">
-      <header className="space-y-1">
-        <h1>Parameters</h1>
-        <span className="text-xs text-slate-300">
-          Check off the parameters you want to use in this task.
-        </span>
+      <header className="flex gap-2">
+        <h1 className="text-xs text-slate-300">Parameters</h1>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <QuestionMarkCircledIcon className="h-4 w-4" />
+            </TooltipTrigger>
+            <TooltipContent>
+              Select the parameters that will be passed to the task.
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </header>
-      <div className="space-y-2">
+      <div className="flex flex-wrap gap-2">
         {keys.map((key) => {
           return (
             <div
               key={key}
-              className="flex items-center gap-2 rounded-sm bg-slate-elevation1 px-3 py-2"
+              className="flex cursor-pointer items-center gap-2 rounded-sm bg-slate-elevation1 px-3 py-2"
+              id={key}
             >
               <Checkbox
                 checked={parameters.includes(key)}
-                onCheckedChange={(checked) => {
-                  if (checked) {
-                    onParametersChange([...parameters, key]);
-                  } else {
-                    onParametersChange(
-                      parameters.filter((parameterKey) => parameterKey !== key),
-                    );
-                  }
+                onCheckedChange={() => {
+                  toggleParameter(key);
                 }}
               />
-              <span className="text-xs">{key}</span>
+              <Label
+                htmlFor={key}
+                className="cursor-pointer text-xs"
+                onClick={() => {
+                  toggleParameter(key);
+                }}
+              >
+                {key}
+              </Label>
             </div>
           );
         })}
