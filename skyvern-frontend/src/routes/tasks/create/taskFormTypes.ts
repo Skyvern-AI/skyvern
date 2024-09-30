59c28f9a02497d24c17c9490689f36a56b53b736
Kerem Yilmaz <kerem@skyvern.com>
2024-09-30 09:08:27 -0700

    Add error code mapping to task (#890)

---
+++ skyvern-frontend/src/routes/tasks/create/taskFormTypes.ts
@@ -0,0 +1,81 @@
+import { z } from "zod";
+
+const createNewTaskFormSchemaBase = z.object({
+  url: z.string().url({
+    message: "Invalid URL",
+  }),
+  webhookCallbackUrl: z.string().or(z.null()),
+  navigationGoal: z.string().or(z.null()),
+  dataExtractionGoal: z.string().or(z.null()),
+  navigationPayload: z.string().or(z.null()),
+  extractedInformationSchema: z.string().or(z.null()),
+  maxStepsOverride: z.number().optional(),
+  totpVerificationUrl: z.string().or(z.null()),
+  totpIdentifier: z.string().or(z.null()),
+  errorCodeMapping: z.string().or(z.null()),
+});
+
+const savedTaskFormSchemaBase = createNewTaskFormSchemaBase.extend({
+  title: z.string().min(1, "Title is required"),
+  description: z.string(),
+  proxyLocation: z.string().or(z.null()),
+});
+
+function refineTaskFormValues(
+  values: CreateNewTaskFormValues | SavedTaskFormValues,
+  ctx: z.RefinementCtx,
+) {
+  const {
+    navigationGoal,
+    dataExtractionGoal,
+    extractedInformationSchema,
+    errorCodeMapping,
+  } = values;
+  if (!navigationGoal && !dataExtractionGoal) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message:
+        "At least one of navigation goal or data extraction goal must be provided",
+      path: ["navigationGoal"],
+    });
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message:
+        "At least one of navigation goal or data extraction goal must be provided",
+      path: ["dataExtractionGoal"],
+    });
+  }
+  if (extractedInformationSchema) {
+    try {
+      JSON.parse(extractedInformationSchema);
+    } catch (e) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Invalid JSON",
+        path: ["extractedInformationSchema"],
+      });
+    }
+  }
+  if (errorCodeMapping) {
+    try {
+      JSON.parse(errorCodeMapping);
+    } catch (e) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Invalid JSON",
+        path: ["errorCodeMapping"],
+      });
+    }
+  }
+}
+
+export const createNewTaskFormSchema =
+  createNewTaskFormSchemaBase.superRefine(refineTaskFormValues);
+
+export const savedTaskFormSchema =
+  savedTaskFormSchemaBase.superRefine(refineTaskFormValues);
+
+export type CreateNewTaskFormValues = z.infer<
+  typeof createNewTaskFormSchemaBase
+>;
+export type SavedTaskFormValues = z.infer<typeof savedTaskFormSchemaBase>;
