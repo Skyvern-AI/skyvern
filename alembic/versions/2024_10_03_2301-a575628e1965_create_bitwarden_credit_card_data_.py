55dee0c7621a9a5529ddd5182fc12833055a811d
Kerem Yilmaz <keremyilmazcs@gmail.com>
2024-10-03 16:01:38 -0700

    Add migration

---
+++ alembic/versions/2024_10_03_2301-a575628e1965_create_bitwarden_credit_card_data_.py
@@ -0,0 +1,23 @@
+"""create bitwarden_credit_card_data_parameters table
+
+Revision ID: a575628e1965
+Revises: 6c90d565076b
+Create Date: 2024-10-03 23:01:05.810580+00:00
+
+"""
+
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = "a575628e1965"
+down_revision: Union[str, None] = "6c90d565076b"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
