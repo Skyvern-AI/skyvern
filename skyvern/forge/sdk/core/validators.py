9a9057ccf7b39689dfa17e7949d3162fa078bc2e
Shuchang Zheng <wintonzheng0325@gmail.com>
2024-10-21 07:59:40 -0700

    SKY-3781 Fix empty URL bug (#1016)

--- skyvern/forge/sdk/core/validators.py
+++ skyvern/forge/sdk/core/validators.py
@@ -8,8 +8,9 @@ from skyvern.exceptions import InvalidUrl
 
 def validate_url(url: str) -> str:
     try:
-        # Use parse_obj_as to validate the string as an HttpUrl
-        parse_obj_as(HttpUrl, url)
+        if url:
+            # Use parse_obj_as to validate the string as an HttpUrl
+            parse_obj_as(HttpUrl, url)
         return url
     except ValidationError:
         # Handle the validation error
