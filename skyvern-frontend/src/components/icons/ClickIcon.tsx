afdb55dec609cb2952c508e386eeb5ec42b3f304
Shuchang Zheng <wintonzheng0325@gmail.com>
2024-12-02 11:46:52 -0800

    use currentColor in custom icons (#1304)

--- skyvern-frontend/src/components/icons/ClickIcon.tsx
+++ skyvern-frontend/src/components/icons/ClickIcon.tsx
@@ -14,7 +14,7 @@ function ClickIcon({ className }: Props) {
     >
       <path
         d="M9 3.5V2M5.06066 5.06066L4 4M5.06066 13L4 14.0607M13 5.06066L14.0607 4M3.5 9H2M8.5 8.5L12.6111 21.2778L15.5 18.3889L19.1111 22L22 19.1111L18.3889 15.5L21.2778 12.6111L8.5 8.5Z"
-        stroke="#F8FAFC"
+        stroke="currentColor"
         strokeWidth="1.6"
         strokeLinecap="round"
         strokeLinejoin="round"
