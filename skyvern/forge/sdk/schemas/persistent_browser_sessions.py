5384eefd451955b37aeb51d55a5399d143613130
Maksim Ivanov <satansdeer@gmail.com>
2025-01-15 16:08:41 +0100

    Add browser address to schema

--- skyvern/forge/sdk/schemas/persistent_browser_sessions.py
+++ skyvern/forge/sdk/schemas/persistent_browser_sessions.py
@@ -10,6 +10,7 @@ class PersistentBrowserSession(BaseModel):
     organization_id: str
     runnable_type: str | None = None
     runnable_id: str | None = None
+    browser_address: str | None = None
     status: str | None = None
     created_at: datetime
     modified_at: datetime
