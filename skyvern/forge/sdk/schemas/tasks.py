d8f9422ed89e785d1564e17f3f4f8e4ce7231105
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-02-01 05:17:33 +0800

    get RESIDENTIAL_DE back which was deleted unexpectedly

--- skyvern/forge/sdk/schemas/tasks.py
+++ skyvern/forge/sdk/schemas/tasks.py
@@ -25,6 +25,7 @@ class ProxyLocation(StrEnum):
     RESIDENTIAL_IN = "RESIDENTIAL_IN"
     RESIDENTIAL_JP = "RESIDENTIAL_JP"
     RESIDENTIAL_FR = "RESIDENTIAL_FR"
+    RESIDENTIAL_DE = "RESIDENTIAL_DE"
     NONE = "NONE"
 
 
@@ -63,10 +64,13 @@ def get_tzinfo_from_proxy(proxy_location: ProxyLocation) -> ZoneInfo | None:
         return ZoneInfo("Asia/Kolkata")
 
     if proxy_location == ProxyLocation.RESIDENTIAL_JP:
-        return ZoneInfo("Asia/Kolkata")
+        return ZoneInfo("Asia/Tokyo")
 
     if proxy_location == ProxyLocation.RESIDENTIAL_FR:
         return ZoneInfo("Europe/Paris")
+
+    if proxy_location == ProxyLocation.RESIDENTIAL_DE:
+        return ZoneInfo("Europe/Berlin")
 
     return None
 
