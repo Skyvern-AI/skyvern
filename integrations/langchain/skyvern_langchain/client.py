c69ab4fa44e439f8cc5068e073375f1e468ff51a
lawyzheng <lawyzheng1106@gmail.com>
2025-02-22 18:50:39 +0800

    refactor v1/v2 interface

--- integrations/langchain/skyvern_langchain/client.py
+++ integrations/langchain/skyvern_langchain/client.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Type
+from typing import Any, Dict, Literal, Type
 
 from httpx import AsyncClient
 from langchain.tools import BaseTool
@@ -12,6 +12,7 @@ from skyvern.forge.sdk.schemas.tasks import CreateTaskResponse, TaskResponse
 class SkyvernClientBaseTool(BaseTool):
     credential: str = ""
     base_url: str = "https://api.skyvern.com"
+    engine: Literal["TaskV1", "TaskV2"] = "TaskV2"
 
     def get_client(self) -> AsyncSkyvern:
         httpx_client = AsyncClient(
@@ -26,12 +27,25 @@ class SkyvernClientBaseTool(BaseTool):
         raise NotImplementedError("skyvern client tool does not support sync")
 
 
-class RunSkyvernClientTaskV1Tool(SkyvernClientBaseTool):
-    name: str = "run-skyvern-client-task-v1"
-    description: str = """Use Skyvern client to run a v1 task. It is usually used for the simple tasks. This function won't return until the task is finished."""
-    args_schema: Type[BaseModel] = TaskV1Request
+class RunSkyvernTaskTool(SkyvernClientBaseTool):
+    name: str = "run-skyvern-client-task"
+    description: str = """Use Skyvern client to run a task. This function won't return until the task is finished."""
+    args_schema: Type[BaseModel] = TaskV2Request
 
-    async def _arun(self, **kwargs: Dict[str, Any]) -> TaskResponse:
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        if self.engine == "TaskV1":
+            self.args_schema = TaskV1Request
+        else:
+            self.args_schema = TaskV2Request
+
+    async def _arun(self, **kwargs: Dict[str, Any]) -> TaskResponse | Dict[str, Any | None]:
+        if self.engine == "TaskV1":
+            return await self._arun_task_v1(**kwargs)
+        else:
+            return await self._arun_task_v2(**kwargs)
+
+    async def _arun_task_v1(self, **kwargs: Dict[str, Any]) -> TaskResponse:
         task_request = TaskV1Request(**kwargs)
         return await self.get_client().agent.run_task(
             max_steps_override=task_request.max_steps,
@@ -52,13 +66,39 @@ class RunSkyvernClientTaskV1Tool(SkyvernClientBaseTool):
             browser_session_id=task_request.browser_session_id,
         )
 
+    async def _arun_task_v2(self, **kwargs: Dict[str, Any]) -> TaskResponse:
+        task_request = TaskV2Request(**kwargs)
+        return await self.get_client().agent.run_observer_task_v_2(
+            max_iterations_override=task_request.max_iterations,
+            user_prompt=task_request.user_prompt,
+            url=task_request.url,
+            browser_session_id=task_request.browser_session_id,
+            webhook_callback_url=task_request.webhook_callback_url,
+            totp_verification_url=task_request.totp_verification_url,
+            totp_identifier=task_request.totp_identifier,
+            proxy_location=task_request.proxy_location,
+        )
 
-class QueueSkyvernClientTaskV1Tool(SkyvernClientBaseTool):
-    name: str = "queue-skyvern-client-task-v1"
-    description: str = """Use Skyvern client to queue a v1 task. It is usually used for the simple tasks. This function will return immediately and the task will be running in the background."""
-    args_schema: Type[BaseModel] = TaskV1Request
 
-    async def _arun(self, **kwargs: Dict[str, Any]) -> CreateTaskResponse:
+class DispatchSkyvernTaskTool(SkyvernClientBaseTool):
+    name: str = "dispatch-skyvern-client-task"
+    description: str = """Use Skyvern client to dispatch a task. This function will return immediately and the task will be running in the background."""
+    args_schema: Type[BaseModel] = TaskV2Request
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        if self.engine == "TaskV1":
+            self.args_schema = TaskV1Request
+        else:
+            self.args_schema = TaskV2Request
+
+    async def _arun(self, **kwargs: Dict[str, Any]) -> CreateTaskResponse | Dict[str, Any | None]:
+        if self.engine == "TaskV1":
+            return await self._arun_task_v1(**kwargs)
+        else:
+            return await self._arun_task_v2(**kwargs)
+
+    async def _arun_task_v1(self, **kwargs: Dict[str, Any]) -> CreateTaskResponse:
         task_request = TaskV1Request(**kwargs)
         return await self.get_client().agent.create_task(
             max_steps_override=task_request.max_steps,
@@ -78,42 +118,7 @@ class QueueSkyvernClientTaskV1Tool(SkyvernClientBaseTool):
             browser_session_id=task_request.browser_session_id,
         )
 
-
-class GetSkyvernClientTaskV1Tool(SkyvernClientBaseTool):
-    name: str = "get-skyvern-client-task-v1"
-    description: str = """Use Skyvern client to get a v1 task. v1 tasks are usually simple tasks."""
-    args_schema: Type[BaseModel] = GetTaskInput
-
-    async def _arun(self, task_id: str) -> TaskResponse:
-        return await self.get_client().agent.get_task(task_id=task_id)
-
-
-class RunSkyvernClientTaskV2Tool(SkyvernClientBaseTool):
-    name: str = "run-skyvern-client-task-v2"
-    description: str = """Use Skyvern client to run a v2 task. It is usually used for the complicated tasks. This function won't return until the task is finished."""
-    args_schema: Type[BaseModel] = TaskV2Request
-
-    async def _arun(self, **kwargs: Dict[str, Any]) -> Dict[str, Any | None]:
-        task_request = TaskV2Request(**kwargs)
-        return await self.get_client().agent.run_observer_task_v_2(
-            max_iterations_override=task_request.max_iterations,
-            timeout_seconds=task_request.timeout_seconds,
-            user_prompt=task_request.user_prompt,
-            url=task_request.url,
-            browser_session_id=task_request.browser_session_id,
-            webhook_callback_url=task_request.webhook_callback_url,
-            totp_verification_url=task_request.totp_verification_url,
-            totp_identifier=task_request.totp_identifier,
-            proxy_location=task_request.proxy_location,
-        )
-
-
-class QueueSkyvernClientTaskV2Tool(SkyvernClientBaseTool):
-    name: str = "queue-skyvern-client-task-v2"
-    description: str = """Use Skyvern client to queue a v2 task. It is usually used for the complicated tasks. This function will return immediately and the task will be running in the background."""
-    args_schema: Type[BaseModel] = TaskV2Request
-
-    async def _arun(self, **kwargs: Dict[str, Any]) -> Dict[str, Any | None]:
+    async def _arun_task_v2(self, **kwargs: Dict[str, Any]) -> Dict[str, Any | None]:
         task_request = TaskV2Request(**kwargs)
         return await self.get_client().agent.observer_task_v_2(
             max_iterations_override=task_request.max_iterations,
@@ -127,10 +132,19 @@ class QueueSkyvernClientTaskV2Tool(SkyvernClientBaseTool):
         )
 
 
-class GetSkyvernClientTaskV2Tool(SkyvernClientBaseTool):
-    name: str = "get-skyvern-client-task-v2"
-    description: str = """Use Skyvern client to get a v2 task. It is usually used for the complicated tasks."""
+class GetSkyvernTaskTool(SkyvernClientBaseTool):
+    name: str = "get-skyvern-client-task"
+    description: str = """Use Skyvern client to get a task."""
     args_schema: Type[BaseModel] = GetTaskInput
 
     async def _arun(self, task_id: str) -> Dict[str, Any | None]:
+        if self.engine == "TaskV1":
+            return await self._arun_task_v1(task_id=task_id)
+        else:
+            return await self._arun_task_v2(task_id=task_id)
+
+    async def _arun_task_v1(self, task_id: str) -> TaskResponse:
+        return await self.get_client().agent.get_task(task_id=task_id)
+
+    async def _arun_task_v2(self, task_id: str) -> Dict[str, Any | None]:
         return await self.get_client().agent.get_observer_task_v_2(task_id=task_id)
