b21a60d349d312dfd80ac10f831148125437e432
lawyzheng <lawyzheng1106@gmail.com>
2025-03-14 03:08:24 +0800

    init the package

---
+++ integrations/n8n/credentials/SkyvernApi.credentials.ts
@@ -0,0 +1,36 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class SkyvernApi implements ICredentialType {
+	name = 'skyvernApi';
+	displayName = 'Skyvern API';
+	// Uses the link to this tutorial as an example
+	// Replace with your own docs links when building your own nodes
+	documentationUrl = 'https://docs.skyvern.ai/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+        {
+            displayName: 'Base URL',
+            name: 'baseUrl',
+            type: 'string',
+            default: 'https://api.skyvern.com',
+        },
+	];
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			qs: {
+				'api_key': '={{$credentials.apiKey}}'
+			}
+		},
+	};
+}
