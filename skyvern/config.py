fc32f917f819ac1d8aa4d73e356c29b5ac34c35c
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-03-23 17:05:38 -0700

    support new browser type: cdp connect (#1875)

--- skyvern/config.py
+++ skyvern/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     ADDITIONAL_MODULES: list[str] = []
 
     BROWSER_TYPE: str = "chromium-headful"
+    BROWSER_REMOTE_DEBUGGING_URL: str = "http://127.0.0.1:9222"
     MAX_SCRAPING_RETRIES: int = 0
     VIDEO_PATH: str | None = "./video"
     HAR_PATH: str | None = "./har"
