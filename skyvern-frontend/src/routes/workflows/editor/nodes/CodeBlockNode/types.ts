c868deca2295f07876d410107272ddbb35b5cea9
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-03-27 18:34:08 -0700

    Add opt-in message to CodeBlock (#2025)

--- skyvern-frontend/src/routes/workflows/editor/nodes/CodeBlockNode/types.ts
+++ skyvern-frontend/src/routes/workflows/editor/nodes/CodeBlockNode/types.ts
@@ -10,6 +10,6 @@ export type CodeBlockNode = Node<CodeBlockNodeData, "codeBlock">;
 export const codeBlockNodeDefaultData: CodeBlockNodeData = {
   editable: true,
   label: "",
-  code: `# All variables will be assigned to the output of this block.\n# Like 'x = 5', 'x' will be assigned to the output of this block.\n\n`,
+  code: `# All variables will be assigned to the output of this block.\n# Like 'x = 5', 'x' will be assigned to the output of this block.\n# This feature is currently in private beta. Please reach out to founders@skyvern.com to get access\n\n`,
   continueOnFailure: false,
 } as const;
