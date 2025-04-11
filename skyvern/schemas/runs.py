2a13ba92fe75bf2ea9d86fd43e4ad14af3932a35
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-04-11 15:45:25 -0700

    fix run_type for TaskRunResponse

--- skyvern/schemas/runs.py
+++ skyvern/schemas/runs.py
@@ -217,8 +217,8 @@ class BaseRunResponse(BaseModel):
 
 
 class TaskRunResponse(BaseRunResponse):
-    run_type: Literal[RunType.task_v1, RunType.task_v2] = Field(
-        description="Type of task run - either task_v1 or task_v2"
+    run_type: Literal[RunType.task_v1, RunType.task_v2, RunType.openai_cua] = Field(
+        description="Types of a task run - task_v1, task_v2, openai_cua"
     )
     run_request: TaskRunRequest | None = Field(
         default=None, description="The original request parameters used to start this task run"
