08df9e5cc6f98b2c28e42d4d9806b1b213c3c59a
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-05-01 00:32:52 -0700

    wrap v2 main_goal with triple backticks

--- skyvern/services/task_v2_service.py
+++ skyvern/services/task_v2_service.py
@@ -65,7 +65,8 @@ DEFAULT_MAX_ITERATIONS = 10
 
 MINI_GOAL_TEMPLATE = """Achieve the following mini goal and once it's achieved, complete: {mini_goal}
 
-This mini goal is part of the big goal the user wants to achieve and use the big goal as context to achieve the mini goal: {main_goal}"""
+This mini goal is part of the big goal the user wants to achieve and use the big goal as context to achieve the mini goal:
+```{main_goal}```"""
 
 
 def _generate_data_extraction_schema_for_loop(loop_values_key: str) -> dict:
