489f8da0d91d8466954bf7ac02a6039c291354a1
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-05-10 01:49:33 -0700

    make additional module loading debug log

--- skyvern/forge/agent.py
+++ skyvern/forge/agent.py
@@ -109,9 +109,9 @@ class ForgeAgent:
     def __init__(self) -> None:
         if settings.ADDITIONAL_MODULES:
             for module in settings.ADDITIONAL_MODULES:
-                LOG.info("Loading additional module", module=module)
+                LOG.debug("Loading additional module", module=module)
                 __import__(module)
-            LOG.info(
+            LOG.debug(
                 "Additional modules loaded",
                 modules=settings.ADDITIONAL_MODULES,
             )
