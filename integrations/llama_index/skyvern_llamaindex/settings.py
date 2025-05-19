7a312b2eafef0823124e80533dcf13b3a4d7854c
lawyzheng <lawyzheng1106@gmail.com>
2025-05-19 16:18:22 +0800

    update llamaindex

--- integrations/llama_index/skyvern_llamaindex/settings.py
+++ integrations/llama_index/skyvern_llamaindex/settings.py
@@ -1,13 +1,13 @@
-from typing import Literal
-
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+from skyvern.schemas.runs import RunEngine
 
 
 class Settings(BaseSettings):
     api_key: str = ""
     base_url: str = "https://api.skyvern.com"
-    engine: Literal["TaskV1", "TaskV2"] = "TaskV2"
+    engine: RunEngine = RunEngine.skyvern_v2
     run_task_timeout_seconds: int = 60 * 60
 
     class Config:
