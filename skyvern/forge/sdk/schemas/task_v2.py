800a26d323e3168ceb720075bba49018e5948b35
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-05-31 18:40:36 -0700

    use model_name instead of name (#2557)

--- skyvern/forge/sdk/schemas/task_v2.py
+++ skyvern/forge/sdk/schemas/task_v2.py
@@ -57,7 +57,7 @@ class TaskV2(BaseModel):
         """
 
         if self.model:
-            model_name = self.model.get("name")
+            model_name = self.model.get("model_name")
             if model_name:
                 mapping = settings.get_model_name_to_llm_key()
                 llm_key = mapping.get(model_name)
