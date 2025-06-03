2dde7f615f7ed5f80ad18270c03198480a9c84ae
Asher Foa <asher@asherfoa.com>
2025-06-03 13:06:02 -0400

    Add shell check to pre-cmmit hooks & fix issues reported by it
    ```
    
    In entrypoint-skyvernui.sh line 2:
    ^-- SC1128 (error): The shebang must be on the first line. Delete blanks and move comments.
    
    In entrypoint-skyvernui.sh line 7:
    export VITE_SKYVERN_API_KEY=$(sed -n 's/.*cred\s*=\s*"\([^"]*\)".*/\1/p' .streamlit/secrets.toml)
           ^------------------^ SC2155 (warning): Declare and assign separately to avoid masking return values.
    
    In entrypoint-skyvern.sh line 2:
    ^-- SC1128 (error): The shebang must be on the first line. Delete blanks and move comments.
    
    In run_alembic_check.sh line 3:
    export PATH=$PATH:.venv/bin
                ^---^ SC2086 (info): Double quote to prevent globbing and word splitting.
    
    Did you mean:
    export PATH="$PATH":.venv/bin
    
    In run_skyvern.sh line 4:
    if [ ! -z "$pid" ]; then
         ^-- SC2236 (style): Use -n instead of ! -z.
    
    In run_skyvern.sh line 5:
      kill $pid
           ^--^ SC2086 (info): Double quote to prevent globbing and word splitting.
    
    Did you mean:
      kill "$pid"
    
    In run_skyvern.sh line 12:
    source "$(poetry env info --path)/bin/activate"
           ^-- SC1091 (info): Not following: ./bin/activate was not specified as input (see shellcheck -x).
    
    For more information:
      https://www.shellcheck.net/wiki/SC1128 -- The shebang must be on the first ...
      https://www.shellcheck.net/wiki/SC2155 -- Declare and assign separately to ...
      https://www.shellcheck.net/wiki/SC1091 -- Not following: ./bin/activate was...
    
    In run_ui.sh line 4:
    if [ ! -z "$pid" ]; then
         ^-- SC2236 (style): Use -n instead of ! -z.
    
    In run_ui.sh line 5:
      kill $pid
           ^--^ SC2086 (info): Double quote to prevent globbing and word splitting.
    
    Did you mean:
      kill "$pid"
    
    In run_ui.sh line 8:
    cd skyvern-frontend
    ^-----------------^ SC2164 (warning): Use 'cd ... || exit' or 'cd ... || return' in case cd fails.
    
    Did you mean:
    cd skyvern-frontend || exit
    
    For more information:
      https://www.shellcheck.net/wiki/SC2164 -- Use 'cd ... || exit' or 'cd ... |...
      https://www.shellcheck.net/wiki/SC2086 -- Double quote to prevent globbing ...
      https://www.shellcheck.net/wiki/SC2236 -- Use -n instead of ! -z.
    
    ```

--- run_ui.sh
+++ run_ui.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 pid=$(lsof -t -i :8080)
-if [ ! -z "$pid" ]; then
-  kill $pid
+if [ -n "$pid" ]; then
+  kill "$pid"
 fi
 
-cd skyvern-frontend
+cd skyvern-frontend || exit 1
 
 if [ ! -f .env ]; then
   cp .env.example .env
