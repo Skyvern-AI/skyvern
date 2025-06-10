26510e08ba1608d5f91e27e6f6947e7cdf729d47
Prakash Maheshwaran <diinoprakash@gmail.com>
2025-06-09 23:52:42 -0400

    Fix formatting and import order in migration

--- alembic/versions/2025_06_10_0348-ad22aca7c6af_mark_onepassword_credential_parameters_.py
+++ alembic/versions/2025_06_10_0348-ad22aca7c6af_mark_onepassword_credential_parameters_.py
@@ -8,12 +8,12 @@ Create Date: 2025-06-10 03:48:07.456369+00:00
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
+import sqlalchemy as sa  # noqa: F401
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'ad22aca7c6af'
-down_revision: Union[str, None] = 'd9ce23f5729c'
+revision: str = "ad22aca7c6af"
+down_revision: Union[str, None] = "d9ce23f5729c"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
