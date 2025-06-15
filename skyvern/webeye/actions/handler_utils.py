26f4273d62e4db05f002a0334bca80f326c2543e
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-06-15 14:32:34 -0700

    fix handler_utils.input_sequentially (#2724)

--- skyvern/webeye/actions/handler_utils.py
+++ skyvern/webeye/actions/handler_utils.py
@@ -27,7 +27,7 @@ async def input_sequentially(locator: Locator, text: str, timeout: float = setti
     if length > TEXT_PRESS_MAX_LENGTH:
         # if the text is longer than TEXT_PRESS_MAX_LENGTH characters, we will locator.fill in initial texts until the last TEXT_PRESS_MAX_LENGTH characters
         # and then type the last TEXT_PRESS_MAX_LENGTH characters with locator.press_sequentially
-        await locator.fill(text, timeout=timeout)
+        await locator.fill(text[: length - TEXT_PRESS_MAX_LENGTH], timeout=timeout)
         text = text[length - TEXT_PRESS_MAX_LENGTH :]
 
     await locator.press_sequentially(text, delay=TEXT_INPUT_DELAY, timeout=timeout)
