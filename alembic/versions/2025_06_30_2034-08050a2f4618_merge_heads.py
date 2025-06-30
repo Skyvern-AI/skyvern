2bf9f968801a7369654f69de766c4f1c6724a6ab
Prakash Maheshwaran <diinoprakash@gmail.com>
2025-06-30 19:00:07 -0400

    Add parameters column to workflow_run_blocks and fix alembic merge

--- alembic/versions/2025_06_30_2034-08050a2f4618_merge_heads.py
+++ alembic/versions/2025_06_30_2034-08050a2f4618_merge_heads.py
@@ -5,15 +5,12 @@ Revises: 760ae45a1345, 6cf2c1e15039
 Create Date: 2025-06-30 20:34:17.004988+00:00
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '08050a2f4618'
-down_revision: Union[str, None] = ('760ae45a1345', '6cf2c1e15039')
+revision: str = "08050a2f4618"
+down_revision: Union[str, None] = ("760ae45a1345", "6cf2c1e15039")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
