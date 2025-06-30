da2de7f0fa13ade372eeaf199d914c6e0c1456b6
Prakash Maheshwaran <diinoprakash@gmail.com>
2025-06-30 18:54:00 -0400

    Update WorkflowRunBlockModel to include 'parameters' field

---
+++ alembic/versions/2025_06_30_2034-08050a2f4618_merge_heads.py
@@ -0,0 +1,26 @@
+"""merge heads
+
+Revision ID: 08050a2f4618
+Revises: 760ae45a1345, 6cf2c1e15039
+Create Date: 2025-06-30 20:34:17.004988+00:00
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '08050a2f4618'
+down_revision: Union[str, None] = ('760ae45a1345', '6cf2c1e15039')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
