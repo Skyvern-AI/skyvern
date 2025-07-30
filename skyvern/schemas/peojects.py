3a58834f2784ac9c516e9c4bd267c7c1c9e44dbc
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-07-30 15:57:12 -0700

    Add projects table (#3063)

---
+++ skyvern/schemas/peojects.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class Project(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    project_revision_id: str
+    project_id: str
+    organization_id: str
+    artifact_id: str | None = None
+    version: int | None = None
+    created_at: datetime
+    modified_at: datetime
+    deleted_at: datetime | None = None
