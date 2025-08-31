3e79f1d970377f9274c4149dba6cfe1e23f4671b
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-08-31 14:19:04 +0800

    add generate_script to the task v2 table

--- skyvern/forge/sdk/db/models.py
+++ skyvern/forge/sdk/db/models.py
@@ -664,6 +664,7 @@ class TaskV2Model(Base):
     max_screenshot_scrolling_times = Column(Integer, nullable=True)
     extra_http_headers = Column(JSON, nullable=True)
     browser_address = Column(String, nullable=True)
+    generate_script = Column(Boolean, nullable=False, default=False)
 
     queued_at = Column(DateTime, nullable=True)
     started_at = Column(DateTime, nullable=True)
