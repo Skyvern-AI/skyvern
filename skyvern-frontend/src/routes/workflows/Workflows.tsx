864f5226e21ac2bd3f57e13a83b79b7bf42837a5
Shuchang Zheng <wintonzheng0325@gmail.com>
2025-09-19 20:30:46 -0700

    Revert "In Workflow Settings, disable the "Generate Code" toggle, and always set it to `true`." (#3488)

--- skyvern-frontend/src/routes/workflows/Workflows.tsx
+++ skyvern-frontend/src/routes/workflows/Workflows.tsx
@@ -50,7 +50,7 @@ const emptyWorkflowRequest: WorkflowCreateYAMLRequest = {
   title: "New Workflow",
   description: "",
   ai_fallback: true,
-  generate_script: true,
+  generate_script: false,
   workflow_definition: {
     blocks: [],
     parameters: [],
