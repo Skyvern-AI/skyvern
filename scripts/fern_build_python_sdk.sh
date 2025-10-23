cf42b9da28343ccf778e588f0d372af625f4ad5b
stas <stas@skyvern.com>
2025-10-23 12:59:58 -0600

    Fix scripts

--- scripts/fern_build_python_sdk.sh
+++ scripts/fern_build_python_sdk.sh
@@ -1,2 +1,2 @@
 fern generate --group python-sdk --log-level debug --preview
-cp -rf fern/.preview/fern-typescript-sdk/* skyvern-ts/client/
+cp -rf fern/.preview/fern-python-sdk/src/skyvern/* skyvern/client/
