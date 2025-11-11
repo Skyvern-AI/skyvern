f2bb0153a44285e511cebdb772501b9e24168630
stas <stas@skyvern.com>
2025-11-10 17:06:08 -0700

    SDK: Disable browser logs

--- skyvern/config.py
+++ skyvern/config.py
@@ -107,6 +107,7 @@ class Settings(BaseSettings):
     BROWSER_WIDTH: int = 1920
     BROWSER_HEIGHT: int = 1080
     BROWSER_POLICY_FILE: str = "/etc/chromium/policies/managed/policies.json"
+    BROWSER_LOGS_ENABLED: bool = True
 
     # Add extension folders name here to load extension in your browser
     EXTENSIONS_BASE_PATH: str = "./extensions"
