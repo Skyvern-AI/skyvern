c377edc50bf8873ede6a7b8697c58297854df397
pedrohsdb <pedro@skyvern.com>
2026-03-15 18:56:58 +0000

    🔄 synced local 'skyvern-frontend/src/' with remote 'skyvern-frontend/src/'

--- skyvern-frontend/src/router.tsx
+++ skyvern-frontend/src/router.tsx
@@ -17,6 +17,7 @@ import { TaskRecording } from "./routes/tasks/detail/TaskRecording";
 import { TasksPage } from "./routes/tasks/list/TasksPage";
 import { Debugger } from "@/routes/workflows/debugger/Debugger";
 import { WorkflowPage } from "./routes/workflows/WorkflowPage";
+import { WorkflowScriptsPage } from "./routes/workflows/WorkflowScriptsPage";
 import { WorkflowRun } from "./routes/workflows/WorkflowRun";
 import { WorkflowRunParameters } from "./routes/workflows/WorkflowRunParameters";
 import { Workflows } from "./routes/workflows/Workflows";
@@ -165,6 +166,10 @@ const router = createBrowserRouter([
               {
                 path: "runs",
                 element: <WorkflowPage />,
+              },
+              {
+                path: "scripts",
+                element: <WorkflowScriptsPage />,
               },
               {
                 path: ":workflowRunId",
