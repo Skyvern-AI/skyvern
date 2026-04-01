ff2369b4608b0fa8ea1ee2dd58d5348f42c85a2a
pedrohsdb <pedro@skyvern.com>
2026-04-01 04:23:34 +0000

    🔄 synced local 'skyvern-frontend/src/' with remote 'skyvern-frontend/src/'

--- skyvern-frontend/src/hooks/useRunsQuery.ts
+++ skyvern-frontend/src/hooks/useRunsQuery.ts
@@ -3,13 +3,19 @@ import { useCredentialGetter } from "@/hooks/useCredentialGetter";
 import { useQuery } from "@tanstack/react-query";
 import { Status, Task, TriggerType, WorkflowRunApiResponse } from "@/api/types";
 
+type QueryReturnType = Array<Task | WorkflowRunApiResponse>;
+type UseQueryOptions = Omit<
+  Parameters<typeof useQuery<QueryReturnType>>[0],
+  "queryKey" | "queryFn"
+>;
+
 type Props = {
   page?: number;
   pageSize?: number;
   statusFilters?: Array<Status>;
   triggerTypeFilters?: Array<TriggerType>;
   search?: string;
-};
+} & UseQueryOptions;
 
 function useRunsQuery({
   page = 1,
@@ -17,11 +23,9 @@ function useRunsQuery({
   statusFilters,
   triggerTypeFilters,
   search,
-  ...queryOptions
 }: Props) {
   const credentialGetter = useCredentialGetter();
   return useQuery<Array<Task | WorkflowRunApiResponse>>({
-    ...queryOptions,
     queryKey: [
       "runs",
       { statusFilters, triggerTypeFilters },
