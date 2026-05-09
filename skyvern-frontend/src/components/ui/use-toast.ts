120c0e54f0c03637acdd22280669add511e13330
andrewneilson <andrew@skyvern.com>
2026-05-09 02:03:49 +0000

    🔄 synced local 'skyvern-frontend/src/' with remote 'skyvern-frontend/src/'

--- skyvern-frontend/src/components/ui/use-toast.ts
+++ skyvern-frontend/src/components/ui/use-toast.ts
@@ -13,13 +13,6 @@ type ToasterToast = Omit<ToastProps, "title"> & {
   action?: ToastActionElement;
 };
 
-const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const;
-
 let count = 0;
 
 function genId() {
@@ -27,7 +20,12 @@ function genId() {
   return count.toString();
 }
 
-type ActionType = typeof actionTypes;
+type ActionType = {
+  ADD_TOAST: "ADD_TOAST";
+  UPDATE_TOAST: "UPDATE_TOAST";
+  DISMISS_TOAST: "DISMISS_TOAST";
+  REMOVE_TOAST: "REMOVE_TOAST";
+};
 
 type Action =
   | {
