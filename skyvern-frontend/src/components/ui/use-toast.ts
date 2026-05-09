aab9ee8f1047851938e34c85f9efd8e37b3ff22b
Andrew Neilson <andrew@skyvern.com>
2026-05-08 17:02:58 -0700

    Fix synced frontend lint disables

--- skyvern-frontend/src/components/ui/use-toast.ts
+++ skyvern-frontend/src/components/ui/use-toast.ts
@@ -13,7 +13,6 @@ type ToasterToast = Omit<ToastProps, "title"> & {
   action?: ToastActionElement;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
