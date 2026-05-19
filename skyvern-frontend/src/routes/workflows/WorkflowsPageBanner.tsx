6e217ccf5bf6b3a9d8f2979497fa25ead47f8618
pedrohsdb <pedro@skyvern.com>
2026-05-19 20:35:17 +0000

    🔄 synced local 'skyvern-frontend/src/' with remote 'skyvern-frontend/src/'

--- skyvern-frontend/src/routes/workflows/WorkflowsPageBanner.tsx
+++ skyvern-frontend/src/routes/workflows/WorkflowsPageBanner.tsx
@@ -9,7 +9,7 @@ const emptyWorkflowRequest: WorkflowCreateYAMLRequest = {
   description: "",
   ai_fallback: true,
   code_version: 2,
-  run_with: "code",
+  run_with: "agent",
   workflow_definition: {
     version: 2,
     blocks: [],
