6807416af6fa6b813e64cfca505d935a0fe190ed
Suchintan Singh <suchintan@skyvern.com>
2026-06-01 16:05:53 +0000

    🔄 synced local 'skyvern-frontend/src/' with remote 'skyvern-frontend/src/'

--- skyvern-frontend/src/routes/workflows/editor/header/EditorOverflowMenu.tsx
+++ skyvern-frontend/src/routes/workflows/editor/header/EditorOverflowMenu.tsx
@@ -1,4 +1,8 @@
-import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import {
+  CounterClockwiseClockIcon,
+  DotsHorizontalIcon,
+  ResetIcon,
+} from "@radix-ui/react-icons";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { type AxiosError } from "axios";
 import { useParams } from "react-router-dom";
@@ -9,6 +13,8 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
@@ -19,21 +25,40 @@ import {
 } from "@/components/ui/tooltip";
 import { toast } from "@/components/ui/use-toast";
 import { useCredentialGetter } from "@/hooks/useCredentialGetter";
+import { statusIsRunningOrQueued } from "@/routes/tasks/types";
 import { useWorkflowQuery } from "@/routes/workflows/hooks/useWorkflowQuery";
+import { useWorkflowRunQuery } from "@/routes/workflows/hooks/useWorkflowRunQuery";
 import { useRecordingStore } from "@/store/useRecordingStore";
 import { useWorkflowHasChangesStore } from "@/store/WorkflowHasChangesStore";
+import { useWorkflowHistoryAccessStore } from "@/store/WorkflowHistoryAccessStore";
 
+import { useUndoRedoShortcutLabels } from "../controls/useUndoRedoShortcutLabels";
 import { useSaveWorkflow } from "../hooks/useSaveWorkflow";
+import { useToggleHistoryPanel } from "../hooks/useToggleHistoryPanel";
+import { CodeSubmenu } from "./CodeSubmenu";
 
 export function EditorOverflowMenu() {
   const { workflowPermanentId } = useParams();
   const { data: workflow } = useWorkflowQuery({ workflowPermanentId });
+  const { data: workflowRun } = useWorkflowRunQuery();
   const isTemplate = workflow?.is_template ?? false;
   const saving = useWorkflowHasChangesStore((s) => s.saveIsPending);
   const isRecording = useRecordingStore((s) => s.isRecording);
   const onSave = useSaveWorkflow();
   const credentialGetter = useCredentialGetter();
   const queryClient = useQueryClient();
+
+  const canUndo = useWorkflowHistoryAccessStore((s) => s.canUndo);
+  const onUndo = useWorkflowHistoryAccessStore((s) => s.undo);
+  const canRedo = useWorkflowHistoryAccessStore((s) => s.canRedo);
+  const onRedo = useWorkflowHistoryAccessStore((s) => s.redo);
+  const { undoShortcutLabel, redoShortcutLabel } = useUndoRedoShortcutLabels();
+
+  const toggleHistoryPanel = useToggleHistoryPanel();
+
+  const workflowRunIsRunningOrQueued = Boolean(
+    workflowRun && statusIsRunningOrQueued(workflowRun),
+  );
 
   const templateMutation = useMutation({
     mutationFn: async (newIsTemplate: boolean) => {
@@ -79,7 +104,7 @@ export function EditorOverflowMenu() {
           <TooltipTrigger asChild>
             <DropdownMenuTrigger asChild>
               <Button
-                disabled={disabled}
+                disabled={isRecording}
                 size="icon"
                 variant="tertiary"
                 className="size-10 min-w-[2.5rem]"
@@ -92,7 +117,40 @@ export function EditorOverflowMenu() {
           <TooltipContent>More actions</TooltipContent>
         </Tooltip>
       </TooltipProvider>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="min-w-[12rem]">
+        <CodeSubmenu />
+        <DropdownMenuItem
+          disabled={!canUndo || isRecording}
+          onSelect={(event) => {
+            event.preventDefault();
+            onUndo();
+          }}
+        >
+          <ResetIcon className="mr-2 size-4" />
+          Undo
+          <DropdownMenuShortcut>{undoShortcutLabel}</DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          disabled={!canRedo || isRecording}
+          onSelect={(event) => {
+            event.preventDefault();
+            onRedo();
+          }}
+        >
+          <ResetIcon className="mr-2 size-4 -scale-x-100" />
+          Redo
+          <DropdownMenuShortcut>{redoShortcutLabel}</DropdownMenuShortcut>
+        </DropdownMenuItem>
+        {!workflowRunIsRunningOrQueued && (
+          <DropdownMenuItem
+            disabled={isRecording}
+            onSelect={() => toggleHistoryPanel()}
+          >
+            <CounterClockwiseClockIcon className="mr-2 size-4" />
+            Version history
+          </DropdownMenuItem>
+        )}
+        <DropdownMenuSeparator />
         <DropdownMenuItem
           disabled={disabled}
           onSelect={(event) => {
