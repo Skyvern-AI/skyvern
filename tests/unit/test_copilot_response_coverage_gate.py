34ab2397e3a1191bf1a122f6ce42a3fe524b2d79
AronPerez <aaron@skyvern.com>
2026-07-31 17:54:46 +0000

    🔄 synced local 'tests/unit/' with remote 'tests/unit/'

--- tests/unit/test_copilot_response_coverage_gate.py
+++ tests/unit/test_copilot_response_coverage_gate.py
@@ -53,6 +53,7 @@ class _Ctx:
         self.composition_page_evidence = None
         self.last_update_block_count = None
         self.last_test_ok = None
+        self.latest_diagnosis_repair_contract = None
         self.last_full_workflow_test_ok = False
         self.last_test_failure_reason = None
         self.last_test_suspicious_success = False
