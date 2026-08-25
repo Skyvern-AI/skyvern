ec158d910dc9fe062c37be15df841bbb6c4dc701
AronPerez <aaron@skyvern.com>
2026-08-25 23:48:03 +0000

    feat: auto-generate alembic migration for sync PR 16083

---
+++ alembic/versions/2026_08_25_2348-88cb50acd5f4_add_cdp_unreachable_at_to_persistent_browser_sessions.py
@@ -0,0 +1,30 @@
+"""add cdp_unreachable_at to persistent_browser_sessions
+
+Revision ID: 88cb50acd5f4
+Revises: bb05ff91073f
+Create Date: 2026-08-25T23:48:03.104718+00:00
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+
+from alembic import op
+from collections.abc import Sequence
+
+# revision identifiers, used by Alembic.
+revision: str = "88cb50acd5f4"
+down_revision: Union[str, None] = "bb05ff91073f"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("SET LOCAL lock_timeout = '5s'")
+    op.add_column("persistent_browser_sessions", sa.Column("cdp_unreachable_at", sa.DateTime(), nullable=True))
+
+
+def downgrade() -> None:
+    op.execute("SET LOCAL lock_timeout = '5s'")
+    op.drop_column("persistent_browser_sessions", "cdp_unreachable_at")
