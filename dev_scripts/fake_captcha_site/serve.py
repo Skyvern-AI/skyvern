165e87ba3296ea8d44dc81abe906209adb902681
Andrew Neilson <andrew@skyvern.com>
2026-09-21 12:40:13 -0700

    [Dev scripts] Fix: annotate fake captcha handler

--- dev_scripts/fake_captcha_site/serve.py
+++ dev_scripts/fake_captcha_site/serve.py
@@ -13,16 +13,17 @@ Pages:
 import http.server
 import os
 import sys
+from typing import Any
 
 PORT = 8888
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
-    def translate_path(self, path):
+    def translate_path(self, path: str) -> str:
         # Google serves the invisible widget's iframe from an extensionless /api2/anchor path.
         if path.split("?", 1)[0] == "/recaptcha/api2/anchor":
             path = "/recaptcha/anchor.html"
